536909c7ad49dbc768c6ef0c84338cae44e52af4
Robin Nunkesser <robin.nunkesser@hshl.de>
2022-02-11 10:51:28 +0100

    0.0.1

--- lib/src/section_tile.dart
+++ lib/src/section_tile.dart
@@ -2,16 +2,19 @@ import 'package:common_ui/common_ui.dart';
 import 'package:flutter/material.dart';
 
 class SectionTile extends StatelessWidget {
-  const SectionTile({Key? key, required this.section}) : super(key: key);
+  const SectionTile(
+      {Key? key, required this.section, this.initiallyExpanded = true})
+      : super(key: key);
 
   final ListSection section;
+  final bool initiallyExpanded;
 
   @override
   Widget build(BuildContext context) {
     return ExpansionTile(
       title: Text(section.title),
       children: section.items.map((item) => ListItemTile(item: item)).toList(),
-      initiallyExpanded: true,
+      initiallyExpanded: initiallyExpanded,
     );
   }
 }
